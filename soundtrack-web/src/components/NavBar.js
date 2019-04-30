a6a383ea3533cad0521c04e4eb47b2fcf50eb29e
AubreyNeimeier <neimeier.1@osu.edu>
2019-04-30 10:58:08 -0700

    final update so repo is most up to date for Alice

--- soundtrack-web/src/components/NavBar.js
+++ soundtrack-web/src/components/NavBar.js
@@ -3,30 +3,19 @@ import { NavLink } from 'react-router-dom'
 // import { logout } from '../actions/sessionActions'
 
 class NavBar extends Component{
-    
-    // handleClick = (e) => {
-    //     debugger
-    //     e.preventDefault()        
-    //     this.props.logout(this.props.history.push('/'))       
-    // }
+   
     render(){
         return (
             <div className="Nav-Bar">
                 <NavLink to="/" className="Nav-Bar-Item" > Login/Signup Page </NavLink>
                 <NavLink to="/entries" className="Nav-Bar-Item" > Entries </NavLink>
-                {/* <NavLink to="entries/new" className="Nav-Bar-Item" > New Entry </NavLink> */}
-                {/* <NavLink to="" className="Nav-Bar-Item" onClick={this.handleClick} > Log Out </NavLink>    */}
-                {/* <NavLink to="/about" className="Nav-Bar-Item" > About </NavLink>          */}
+                
             </div> 
         )
     }
     }
         
-    // const mapDispatchToProps = dispatch => bindActionCreators({logout}, dispatch)
+   
 
 export default NavBar
-// export default withRouter(connect(null, mapDispatchToProps)(NavBar))
-
-// {props.entry.date} 
-// 
-                
+          
